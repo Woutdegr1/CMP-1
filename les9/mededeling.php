275b245a7c491e72339cc9412eb8fc94648aa248
evelienrutsaert <rutsaert@MacBook-Air-van-Rutsaert.local>
2016-11-29 11:11:38 +0100

    [WIP]

---
+++ les9/mededeling.php
@@ -0,0 +1,16 @@
+Maak een nieuwe pagina insert_student.php
+Hergebruik het formulier van edit_student.php
+Maak gebruik van de prepared statement
+Voeg de nieuwe pagina toe aan je menu
+Informeer jezelf : 
+- gdm.gent/mod_php
+- http://php.net/
+
+
+
+Vergeet niet:
+Het formulier i.v.m de eindopdracht in te vullen. 
+Indien je nog geen onderwerp hebt, vul het toch maar in!
+Onderwerp is niet definitief :)
+
+!PHP RULES!
