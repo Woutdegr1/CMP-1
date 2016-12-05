6b4010a2ecaf735fdfc90da8237c71765172d2d7
evelienrutsaert <rutsaert@MacBook-Air-van-Rutsaert.local>
2016-12-05 11:47:53 +0100

    [WIP]

--- les10/students/insert_student.php
+++ les10/students/insert_student.php
@@ -7,18 +7,24 @@ if(isset($_POST['new'])){
 	//GEKLIKT OP EDITKNOP
 	require("connectie.php");
 
+	$target_dir = "profilepictures/";
+    $target_file = $target_dir . $_FILES["profilepicture"]["name"];
+    move_uploaded_file($_FILES["profilepicture"]["tmp_name"], $target_file);
+
 	$naam = $_POST["naam"];
 	$familienaam = $_POST["familienaam"];
 	$email = $_POST["email"];
-
+	$profilepicture_name = basename($_FILES["profilepicture"]["name"]);
+	echo $profilepicture_name;
 	try{
 		$stmt = $db->prepare("INSERT INTO students 
-                        (naam, familienaam, email)
+                        (naam, familienaam, email, profilepicture)
                         VALUES
-                        (:naam, :familienaam, :email)");
+                        (:naam, :familienaam, :email, :profilepicture)");
 		$stmt->bindParam(":naam", $naam);
 		$stmt->bindParam(":familienaam", $familienaam);
 		$stmt->bindParam(":email", $email);
+		$stmt->bindParam(":profilepicture", $profilepicture_name);
 
 		$stmt->execute();
 		$message = "SUCCES";
@@ -40,10 +46,10 @@ if(isset($_POST['new'])){
 
 <div class="container">
     <div class="row">
-		<form role="form" class="col-md-9 go-right" action="insert_student.php" method="post">
+		<form role="form" class="col-md-9 go-right" action="insert_student.php" method="post" enctype="multipart/form-data">
 			<h2>Edit Student</h2>
 		<div class="form-group">
-			<input id="naam" name="naam" type="text" class="form-control" required >
+			<input id="naam" name="naam" type="text" class="form-control" required>
 			<label for="naam">Naam</label>
 		</div>
 		<div class="form-group">
@@ -54,9 +60,13 @@ if(isset($_POST['new'])){
 			<input id="email" name="email" type="email" class="form-control" required >
 			<label for="email">Email</label>
 		</div>
-		
 		<div class="form-group">
-			<input id="new" name="new" type="submit" class="form-control" value="Voeg Toe">
+			<input id="profilepicture" name="profilepicture" type="file" class="form-control"  >
+			<label for="profilepicture">Profielfoto</label>
+		</div>
+
+		<div class="form-group">
+			<input id="new" name="new" type="submit" class="form-control" value="Maak nieuwe aan">
 			
 		</div>
 		</form>
@@ -70,5 +80,4 @@ if(isset($_POST['new'])){
 	</div>
 	<a class="btn btn-primary" href="studenten.php">Overzicht studenten</a>
 </div>
- 
 <?php require_once("includes/footer.php"); ?>
